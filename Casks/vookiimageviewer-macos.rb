7855cc1e7d206df1d881239ef5440947076f72b1
Michal Duda <github@vookimedlo.cz>
2019-04-07 22:18:19 +0200

    Version: v2019.04.07

---
+++ Casks/vookiimageviewer-macos.rb
@@ -0,0 +1,10 @@
+cask 'vookiimageviewer-macos' do
+version '2019.04.07'
+url 'https://github.com/vookimedlo/vooki-image-viewer/releases/download/v2019.04.07/VookiImageViewer-brew-macos.zip'
+sha256 'eb1e4981911b0857650ba84b188d5fac15612229f619a67b33e027583f4b692a'
+
+name 'VookiImageViewer'
+homepage "https://github.com/vookimedlo/vooki-image-viewer"
+
+app 'VookiImageViewer.app'
+end
