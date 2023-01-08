3a31e6d3662403d308e4b7212337229e3d41bdc0
Michal Duda <github@vookimedlo.cz>
2023-01-08 22:43:00 +0100

    🏆Release: v2023.01.08

--- Casks/vookiimageviewer-macos.rb
+++ Casks/vookiimageviewer-macos.rb
@@ -1,7 +1,7 @@
 cask 'vookiimageviewer-macos' do
-version '2022.10.13'
-url 'https://github.com/vookimedlo/vooki-image-viewer/releases/download/v2022.10.13/VookiImageViewer-brew-macos.zip'
-sha256 'ff4b9509cdc247582d928e78ea9c3cd85a1d731729b02c6129f830f5e91cb067'
+version '2023.01.08'
+url 'https://github.com/vookimedlo/vooki-image-viewer/releases/download/v2023.01.08/VookiImageViewer-brew-macos.zip'
+sha256 '554668caa58e6118bbac772f6b1eb8a7416e3ca5f26d8a10be0cf7368d9d96a5'
 
 name 'VookiImageViewer'
 homepage "https://github.com/vookimedlo/vooki-image-viewer"
